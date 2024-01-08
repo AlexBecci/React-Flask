736de6309702a03b8d55283a05a3dcb98760f9a7
Alex Becci <alex.becci@bingopilar.corp>
2024-01-08 17:44:35 -0300

    TEST

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from 'react';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [profileData, setProfileData] = useState(null);
+
+  function getData() {
+    fetch("/profile")
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`Network response was not ok: ${response.status}`);
+        }
+        return response.json(); // Parse the response body as JSON
+      })
+      .then((data) => {
+        setProfileData({
+          profile_name: data.name,
+          about_me: data.about,
+        });
+        console.log(data); // Log the parsed JSON data
+      })
+      .catch((error) => {
+        console.error("Error during fetch:", error.message);
+        return response.text(); // Log the response text for further analysis
+      })
+      .then((text) => {
+        console.log("Response text:", text);
+      });
+  }
+
+  useEffect(() => {
+    getData();
+  }, []);
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1>HOLA</h1>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
